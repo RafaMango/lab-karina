6ca05600e82a90ccda3203f11075e2f034ea4f90
RafaMango <rafaelabascunan2005@gmail.com>
2025-08-16 10:27:08 -0400

    sirve porfa

--- lab-1/exercises.c
+++ lab-1/exercises.c
@@ -41,8 +41,16 @@ debes reservar memoria para cada elemento que agregues.
 Al finalizar retorna la lista creada.
 */
 
-List* crea_lista() {
+List* crea_lista() 
+{
    List* L = create_list();
+
+   for(int i = 1; i <= 10; i++)
+   {
+      int* num = (int*)malloc(sizeof(int));
+      *num = i;
+      pushBack(L, num);
+   }
    return L;
 }
 
